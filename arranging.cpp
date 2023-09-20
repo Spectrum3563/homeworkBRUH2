6fe879825428c008ef7cd326194dc3c9c472e747
tbq2107 <quan.tb.2340@aptechlearning.edu.vn>
2023-09-20 20:49:32 +0700

    gokisoft 1910

---
+++ arranging.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	cout << "Nhap so n: ";
+	int n;cin >> n;int t[n];
+	cout << "\nNhap mang t gom n so: ";
+	for(int i=0;i<n;i++){
+		cin >> t[i];
+	}
+	n=sizeof(t)/sizeof(t[0]);
+	sort(t, t+n);
+	cout << "\nMang t sau khi duoc sap xep: ";
+	for(int i=0;i<n;i++){
+		cout << t[i] << " ";
+	}
+}
